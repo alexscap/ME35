dfa75cdafd900d6ef16faa04ba10835b552581a1
alexscap <60908866+alexscap@users.noreply.github.com>
2020-03-02 23:10:24 -0500

    Update throwerAI.py

--- throwerAI.py
+++ throwerAI.py
@@ -1,5 +1,7 @@
 from numpy import *
 
+#training data arrays
+
 dist = array([94, 157, 191, 370, 135, 130, 303, 380, 348, 380, 103, 173, 265, 145, 191, 249, 330, 410, 496, 251])
 speed = array([200, 300, 350, 600, 300, 350, 450, 600, 550, 600, 200, 300, 450, 300, 350, 450, 550, 650, 700, 400])
 
@@ -8,6 +10,7 @@ speed_bounce = array([150, 250, 300, 350, 500, 600, 150, 250, 300, 400, 500, 650
 
 from scipy.interpolate import *
 
+#fit line to the data
 fit = polyfit(dist, speed, 1)
 fit_bounce = polyfit(dist_bounce, speed_bounce, 1)
 
@@ -17,6 +20,7 @@ print(fit)
 from matplotlib.pyplot import *
 %matplotlib inline
 
+#plot line and training data
 plot(dist, speed, 'o')
 plot(dist, polyval(fit, dist), 'r-')
 
